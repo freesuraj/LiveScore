a1ed547d06b1ce9a93ee1b4e6e168b242395d22f
Suraj Pathak <suraj.pathak@catch.com.au>
2025-02-07 20:17:50 +1100

    Initial Commit

---
+++ LiveScore/LiveScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScoreApp.swift
+//  LiveScore
+//
+//  Created by Suraj Pathak on 7/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct LiveScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveScoreTests/LiveScoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScoreTests.swift
+//  LiveScoreTests
+//
+//  Created by Suraj Pathak on 7/2/2025.
+//
+
+import Testing
+@testable import LiveScore
+
+struct LiveScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
